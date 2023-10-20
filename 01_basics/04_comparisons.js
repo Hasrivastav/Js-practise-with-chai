6b4583d1de5280006384bc32368b7ea2907572ca
HARSH SRIVASTAV <96014384+Hasrivastav@users.noreply.github.com>
2023-10-20 14:26:36 +0000

    datarypes

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,11 +1,19 @@
-console.log(null >0 )
-console.log(null == 0)
-console.log(null>=0)
+// console.log(null >0 )
+// console.log(null == 0)
+// console.log(null>=0)
+// // 
 
 // Reson
 // in js comparison  and equality check  ==  works diffrently
 // comparison convert null to a number , treating it as 0.
 // that why null >=0 is true adn null > 0 is false
 
+let myName = "Harsh";
+let anotherName = myName;
 
-//=== strict check  it check the value and datatype of the operands
+console.log(myName);
+console.log(anotherName);
+
+
+
+  
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+let myName = "Harsh";
+// let anotherName = myName;
+
+console.log(myName);
+// console.log(anotherName);
+
+
+
+//primitive 
+//7 types  Number,String, Boolean,undefined,null,bigInt,Symbol
+
+//non-primitve object,array, functions
+
+//********memorytype*/
+
+//stack(primitive) we get a copy of the values passed
+
+ //heap(non-primitive) we get the refrence of the value passed
+
+
+
